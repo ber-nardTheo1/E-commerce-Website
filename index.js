732ee667ac27c330e0e2a869d31de5460d81b92d
Bernard Malunga <70895887+ber-nardTheo1@users.noreply.github.com>
2023-03-20 22:33:57 +0900

    commit

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const subMitBtn = document.querySelector('.submit-btn')
 const termsAndconditions = document.querySelector('#terms-and-conds')
 
 subMitBtn.addEventListener('click', ()=>{
-    if(!FirstName.value.lenght){
+    if(FirstName===""){
         showAlert("Fill your first name")
     }
-    else if(!LastName.value.length){
+    else if(LastName===""){
         showAlert("Fill your last name")
     }
-    else if(!email.value.length){
+    else if(email===""){
         showAlert("enter your email")
     }
 
@@ -36,6 +36,36 @@ const showAlert = (msg) =>{
     }, 3000);
 
 }
+
+const SendData = (path, data)=>{
+    fetch(path, {
+        method: "post",
+        headers: new Headers({'content-Type': 'application/json'}),
+        body: JSON.stringify(data)
+    }).then((res) => res.json())
+    .then(response =>{
+        processData(response)
+    })
+
+    // submit form
+    loader.style.display = 'block';
+    SendData('/signup', {
+        FirstName: FirstName.value,
+        LastName: LastName,
+        email: email.value,
+        passWord: passWord.value,
+        termsAndconditions: termsAndconditions.checked,
+        seller: false
+    })
+
+  
+
+}
+
+
+    
+
+
 
 
 
