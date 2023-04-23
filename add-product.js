1c30b53638f4ba465ddb7248ebb7a9b9b70d16ad
Bernard Malunga <70895887+ber-nardTheo1@users.noreply.github.com>
2023-04-23 09:39:36 +0900

    commit

---
+++ add-product.js
@@ -0,0 +1,13 @@
+//let user = JSON.parse(sessionStorage.user || null)
+
+// checking user is logged in or not 
+
+//window.onload =() =>{
+   // if(user){
+      //  if(!compareToken(user.authToken, user.email)){
+        //    location.replace("/login")
+      //  } 
+   // } else{
+      //  location.replace("/login")
+    //}
+//}
